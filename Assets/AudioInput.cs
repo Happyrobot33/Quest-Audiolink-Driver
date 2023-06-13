f0bfaef7e616cf65206eb5bd4521af347d25a74d
Happyrobot33 <matthewherber@sbcglobal.net>
2023-06-13 08:32:09 -0400

    Update before unity upgrade for apriltags

---
+++ Assets/AudioInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioInput : MonoBehaviour
+{
+    public string deviceName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(deviceName, true, 10, 44100);
+        audioSource.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audioSource.Play();
+    }
+}
---
+++ Assets/CopyToRenderTex.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyToRenderTex : MonoBehaviour
+{
+    public RenderTexture renderTexture;
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the material from the renderer
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //copy the material to the render texture, flipping it vertically
+        Graphics.Blit(material.mainTexture, renderTexture, new Vector2(1, -1), new Vector2(0, 1));
+    }
+}
